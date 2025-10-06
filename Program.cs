48107de1791d0936827de61b0e1405993c651b04
luan-rodrigues1 <rodrigues.luan20@gmail.com>
2025-10-06 20:31:35 -0300

    chore: add cors configuration to allow cross-origin requests

--- Program.cs
+++ Program.cs
@@ -21,6 +21,16 @@ builder.Services.AddScoped<ITransactionInterface, TransactionService>();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
